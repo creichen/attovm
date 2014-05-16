2f3f211581be4ce4b4f42716906808215debf053
Christoph Reichenbach <creichen@gmail.com>
2014-05-16 07:39:17 +0000

    Added Makefile and test program source

---
+++ src/parse-test.c
@@ -0,0 +1,47 @@
+/***************************************************************************
+  Copyright (C) 2014 Christoph Reichenbach
+
+
+ This program may be modified and copied freely according to the terms of
+ the GNU general public license (GPL), as long as the above copyright
+ notice and the licensing information contained herein are preserved.
+
+ Please refer to www.gnu.org for licensing details.
+
+ This work is provided AS IS, without warranty of any kind, expressed or
+ implied, including but not limited to the warranties of merchantability,
+ noninfringement, and fitness for a specific purpose. The author will not
+ be held liable for any damage caused by this work or derivatives of it.
+
+ By using this source code, you agree to the licensing terms as stated
+ above.
+
+
+ Please contact the maintainer for bug reports or inquiries.
+
+ Current Maintainer:
+
+    Christoph Reichenbach (CR) <creichen@gmail.com>
+
+***************************************************************************/
+
+#include<stdio.h>
+#include<ast.h>
+#include<parser.h>
+
+int
+main(int argc, char **argv)
+{
+	yyin = stdin;
+
+	ast_node_t *n = NULL;
+	if (!parse_expr(&n)) {
+		fprintf(stderr, "Parse error");
+		return 1;
+	}
+
+	ast_node_dump(stdout, n, 1);
+	ast_node_free(n);
+
+	return 0;
+}

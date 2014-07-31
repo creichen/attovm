295c0866dff681dda268f8af662e5c5764d80a3c
Christoph Reichenbach <creichen@gmail.com>
2014-07-31 09:39:06 +0000

    Added missing file

---
+++ src/compiler-options.h
@@ -0,0 +1,45 @@
+/***************************************************************************
+  Copyright (C) 2014 Christoph Reichenbach
+
+
+ This program may be modified and copied freely according to the terms of
+ the GNU general public license (GPL), as long as the above copyright
+ notice and the licensing information contained herein are preserved.
+
+ Please refer to www.gnu.org for licensing details.
+
+ This work is provided AS IS, without warranty of any kind, expressed or
+ implied, including but not limited to the warranties of merchantability,
+ noninfringement, and fitness for a specific purpose. The author will not
+ be held liable for any damage caused by this work or derivatives of it.
+
+ By using this source code, you agree to the licensing terms as stated
+ above.
+
+
+ Please contact the maintainer for bug reports or inquiries.
+
+ Current Maintainer:
+
+    Christoph Reichenbach (CR) <creichen@gmail.com>
+
+***************************************************************************/
+
+#ifndef _ATTOL_COMPILER_OPTIONS_H
+#define _ATTOL_COMPILER_OPTIONS_H
+
+#include <stdbool.h>
+#include "ast.h"
+
+struct compiler_options {
+	bool no_bounds_checks; // Keine Arrayschrankenpruefung
+	bool debug_dynamic_compilation; // Drucke Ausgaben aus, waehrend dynamische Uebersetzung aktiv ist
+
+	int array_storage_type;
+	int method_call_param_type;
+	int method_call_return_type;
+};
+
+extern struct compiler_options compiler_options;
+
+#endif // !defined(_ATTOL_COMPILER_OPTIONS_H)
